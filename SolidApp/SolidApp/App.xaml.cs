cc40ef1b0878500e52c01cbb58d725cc01d37206
Azamat Davletbaev <azamat.davletbaev@gmail.com>
2024-02-14 21:36:22 +0500

    second

---
+++ SolidApp/SolidApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SolidApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
